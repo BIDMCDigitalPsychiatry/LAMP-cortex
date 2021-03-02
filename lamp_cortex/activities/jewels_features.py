2e1cb5bdac99adf1c7b0e37e58a92d766e38e02f
Ryan Hays <ryan.hays28@gmail.com>
2021-03-02 22:01:34 +0000

    Jewels features fixed and integrated

--- lamp_cortex/activities/jewels_features.py
+++ lamp_cortex/activities/jewels_features.py
@@ -11,13 +11,27 @@ def jewels_featurize(results, times, resolution, key):
     if key not in results:
         return pd.DataFrame({'Date':times})
 
+    JEWELS_FEATURES = ['duration', 'point', 'score', 'total_attempts', 'total_bonus_collected', 'total_jewels_collected']
     traildat = results[key]
     stat = pd.DataFrame(traildat.static_data.tolist())
     sel_jewels_data = traildat.join(stat)
 
     column_labels = traildat.static_data.tolist()
 
-    return sel_jewels_data
+    timesSeries = pd.Series(times)
+    time_sel_jewels = sel_jewels_data.apply(lambda row: timesSeries[(timesSeries <= row['local_datetime']) & ((row['local_datetime'] - timesSeries) < resolution)].max(), axis=1)
+    
+    sel_jewels_data.loc[:, 'matched_time'] = time_sel_jewels 
+
+    #For each window, calculate entropy
+    jewels_data = []
+    for t in times:
+        t_jewels = sel_jewels_data.loc[sel_jewels_data['matched_time'] == t, :].reset_index().sort_values(by='local_datetime')
+        t_data = [t] + t_jewels[JEWELS_FEATURES].mean().tolist() + [t_jewels.static_data.values]
+        jewels_data.append(t_data)
+
+    jewelsDf = pd.DataFrame(jewels_data, columns=['Date'] + ['.'.join([key, f]) for f in JEWELS_FEATURES + ['static_data']])
+    return jewelsDf
 
 
 def tempfunc(results, date_list, resolution):
