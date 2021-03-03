89608254cb4cc6f6712f2884ed913e24398e84c2
Ryan Hays <ryan.hays28@gmail.com>
2021-03-03 01:09:25 +0000

    More call fixes

--- lamp_cortex/sensors/call_text_features.py
+++ lamp_cortex/sensors/call_text_features.py
@@ -8,7 +8,7 @@ def call_degree(data, times, resolution):
     Find degree of call log (i.e. how many persons the user connected with over phone)
     """
     if 'lamp.calls' not in data:
-        return pd.DataFrame({'Date': times, 'Call Degree': [0] * len(times)})
+        return pd.DataFrame({'Date': times})
         
     #Remove duplicate entries by converting call data to set
     call_data = data['lamp.calls']
@@ -18,10 +18,10 @@ def call_degree(data, times, resolution):
     timesSeries = pd.Series(times)
     time_sel_call_data = sel_call_data.apply(lambda row: timesSeries[(timesSeries <= row['local_datetime']) & ((row['local_datetime'] - timesSeries) < resolution)].max(), axis=1)
     
-    sel_call_data.loc[:, 'time'] = time_sel_call_data.values
-
     if len(time_sel_call_data) == 0:
-        return pd.DataFrame([[t, 0] for t in times], columns=column_labels)
+        return pd.DataFrame([[t, 0] for t in times], columns=['Date', 'Call Degree'])
+        
+    sel_call_data.loc[:, 'time'] = time_sel_call_data.values
 
     callDegreeDf = pd.DataFrame([[t, sel_call_data.loc[sel_call_data['time'] == t, 'call_trace'].nunique()] for t in times], columns=['Date', 'Call Degree'])
     
@@ -39,24 +39,25 @@ def call_duration(data, times, resolution, label=1):
     :param label(int): indicates whether or not to query incoming calls (1) or outgoing calls (2)
     """
     if 'lamp.calls' not in data:
-        return pd.DataFrame({'Date': times, 'Call Duration': [0] * len(times)})
+        return pd.DataFrame({'Date': times})
         
     #Remove duplicate entries by converting call data to set
     call_data = data['lamp.calls']
     sel_call_data = call_data.loc[call_data['call_type'] == label, :]
+
+    #Set labels
+    if label == 1: column_labels = ['Date', 'Call Duration.Incoming']
+    elif label == 2: column_labels = ['Date', 'Call Duration.Outgoing']
+    else: column_labels = ['Date', '.'.join(['Call Duration', 'Unknown', str(label)])]
     
     #Map each call with corresponding time window
     timesSeries = pd.Series(times)
     time_sel_call_data = sel_call_data.apply(lambda row: timesSeries[(timesSeries <= row['local_datetime']) & ((row['local_datetime'] - timesSeries) < resolution)].max(), axis=1)
-    
-    sel_call_data.loc[:, 'time'] = time_sel_call_data
 
     if len(time_sel_call_data) == 0:
         return pd.DataFrame([[t, 0] for t in times], columns=column_labels)
-    
-    if label == 1: column_labels = ['Date', 'Call Duration.Incoming']
-    elif label == 2: column_labels = ['Date', 'Call Duration.Outgoing']
-    else: column_labels = ['Date', '.'.join(['Call Duration', 'Unknown', str(label)])]
+
+    sel_call_data.loc[:, 'time'] = time_sel_call_data
 
     callDurationDf = pd.DataFrame([[t, sel_call_data.loc[sel_call_data['time'] == t, 'call_duration'].mean()] for t in times], columns=column_labels)
     
@@ -77,7 +78,7 @@ def call_number(data, times, resolution, label=1):
     else: column_labels = ['Date', '.'.join(['Call Number', 'Unknown', str(label)])]
 
     if 'lamp.calls' not in data:
-        return pd.DataFrame([[t, 0] for t in times], columns=column_labels)
+        return pd.DataFrame({'Date': times})
         
     #Remove duplicate entries by converting call data to set
     call_data = data['lamp.calls']
