a9467f00dba5ca4bdfd11706a9c040e1915b6e7b
Ryan Hays <ryan.hays28@gmail.com>
2021-03-19 17:24:56 +0000

    Sleep periods/duration added

--- cortex/primary/sleep_periods.py
+++ cortex/primary/sleep_periods.py
@@ -1,4 +1,6 @@
 from ..feature_types import primary_feature
+from ..raw.accelerometer import accelerometer
+
 import numpy as np
 import pandas as pd 
 import math 
@@ -8,71 +10,68 @@ from functools import reduce
 
 @primary_feature(
     name="cortex.feature.sleep_periods",
-    dependencies=['lamp.accelerometer']
+    dependencies=[accelerometer]
 )
-def sleep_periods(sensor_data):
+def sleep_periods(**kwargs):
     """
     Generate sleep periods with given data
     """
-    #Find maximum bout of inactivity; that's mean sleep period 
-    times = [(datetime.time(hour=h, minute=m).strftime("%H:%M"), (datetime.datetime.combine(datetime.date.today(), datetime.time(hour=h, minute=m)) + datetime.timedelta(hours=8, minutes=0)).strftime("%H:%M")) for h in range(18, 24)  for m in [0, 30] ] + [(datetime.time(hour=h, minute=m).strftime("%H:%M"), (datetime.datetime.combine(datetime.date.today(), datetime.time(hour=h, minute=m)) + datetime.timedelta(hours=8, minutes=0)).strftime("%H:%M")) for h in range(0, 4) for m in [0, 30] ]
-    mean_activity = float('inf')
-    for t0, t1 in times:
-        if "18:00" <= t0 <= "23:00":
-            selection = pd.concat([df.loc[datetime.datetime.strptime(t0, "%H:%M").time() <= df['Time'], :], df.loc[df['Time'] <= datetime.datetime.strptime(t1, "%H:%M").time(), :]])
-        else:
-            selection = df.loc[(datetime.datetime.strptime(t0, "%H:%M").time() <= df['Time']) & (df['Time'] <= datetime.datetime.strptime(t1, "%H:%M").time()), :]
+    def expected_sleep_period(accelerometer_data):
+        """
+        Return the expected sleep period for a set of accelerometer data
 
-        sel_act = selection['magnitude'].abs().mean()
+        :param accelerometer_data (list)
 
-        if sel_act < mean_activity:
-            mean_activity = sel_act
-            period = (t0, t1, sel_act)
-         
-    if mean_activity == float('inf'):
-        return pd.DataFrame([[np.nan, np.nan, sel_act]], columns=['Bed', 'Wake', 'Accelerometer Mag'])
-        
-    return pd.DataFrame([[t0, t1, sel_act]], columns=['Bed', 'Wake', 'Accelerometer Mag'])
-    
-def sleep_time_mean(sensor_data, dates):
-    """
-    Return mean expected bed and wake time
-    """
-    accelDf = convert_to_df(sensor_data)
-    bed_raw, wake_raw, _ = get_optimal_inactivity(accelDf)
-    if (bed_raw, wake_raw) == (None, None):
-        return (None, None)
-    bed, wake = pd.to_datetime(bed_raw, format= '%H:%M' ).time(), pd.to_datetime(wake_raw, format= '%H:%M' ).time()
-    return bed, wake
+        :return _sleep_period_expted (dict): list bed/wake timestamps and mean accel. magnitude for expected bedtime
+        """
+        #Find maximum bout of inactivity; that's mean sleep period 
+        df = pd.DataFrame.from_dict(accelerometer_data)
+        df.loc[:, 'Time'] = pd.to_datetime(df['timestamp'], unit='ms')
+        times = [(datetime.time(hour=h, minute=m), (datetime.datetime.combine(datetime.date.today(), datetime.time(hour=h, minute=m)) + datetime.timedelta(hours=8, minutes=0)).time()) for h in range(18, 24)  for m in [0, 30] ] + [(datetime.time(hour=h, minute=m), (datetime.datetime.combine(datetime.date.today(), datetime.time(hour=h, minute=m)) + datetime.timedelta(hours=8, minutes=0)).time()) for h in range(0, 4) for m in [0, 30] ]
+        mean_activity = float('inf')
+        for t0, t1 in times:
+            if datetime.time(hour=18, minute=0) <= t0 <= datetime.time(hour=23, minute=30):
+                selection = pd.concat([df.loc[t0 <= df['Time'].dt.time, :], df.loc[df['Time'].dt.time <= t1, :]])
+                
+            else:
+                selection = df.loc[(t0 <= df['Time'].dt.time) & (df['Time'].dt.time <= t1), :]
 
-def activities(sensor_data, dates):
-    """
-    Get percentage of sleep, active, and sedentary for the patient
-    """
-    TIME = dates[0].time()
-    accelDf = convert_to_df(sensor_data)
-    bed_time, wake_time = sleep_time_mean(sensor_data, dates)
-    if (bed_time, wake_time) == (None, None):
-        return pd.DataFrame({"Date": dates,
-                             "Sleep Duration": [None] * len(dates),
-                             "Sedentary Duration": [None] * len(dates), 
-                             "Activity Duration": [None] * len(dates)})
+            sel_act = selection.apply(lambda row: np.linalg.norm([row['x'], row['y'], row['z']]), axis=1).abs().mean()
+            if sel_act < mean_activity:
+                mean_activity = sel_act
+                _sleep_period_expected = {'bed':t0 ,'wake':t1, 'accelerometer_magnitude':sel_act} 
+            
+        if mean_activity == float('inf'):
+            _sleep_period_expected = {'bed':None, 'wake':None, 'accelerometer_magnitude':None}
+            
+        return _sleep_period_expected
 
+
+    ### ###
+    accelerometer_data = accelerometer(**kwargs)
+    _sleep_period_expected = expected_sleep_period(accelerometer_data)
+    if _sleep_period_expected['bed'] == None:
+        return []
+
+    accelDf = pd.DataFrame.from_dict(accelerometer_data)
+    accelDf.loc[:, 'Time'] = pd.to_datetime(accelDf['timestamp'], unit='ms')
+    accelDf.loc[:, 'magnitude'] = accelDf.apply(lambda row: np.linalg.norm([row['x'], row['y'], row['z']]), axis=1)
     #Get mean accel readings of 10min bins for participant
-    times, magnitudes = [], []
-    accelDf.loc[:, 'Time'] = pd.to_datetime(accelDf["Time"].astype(str))
+    data_10min = []
     for s, t in accelDf.groupby(pd.Grouper(key='Time', freq='10min')):
-        times.append(s.time())
-        magnitudes.append(t['magnitude'].abs().mean())
+        res_10min = {'time': s.time(), 'magnitude': t['magnitude'].abs().mean()}
+        data_10min.append(res_10min)
 
-    df10min = pd.DataFrame({'Time':times, 'Magnitude':magnitudes}).sort_values(by='Time')
+    df10min = pd.DataFrame.from_dict(data_10min).sort_values(by='time')
+    
+    bed_time, wake_time = _sleep_period_expected['bed'], _sleep_period_expected['wake']
     
     #Calculate sleep
     sleepStart, sleepStartFlex = bed_time, (datetime.datetime.combine(datetime.date.today(), bed_time) - datetime.timedelta(hours=2)).time()
     sleepEnd, sleepEndFlex = wake_time, (datetime.datetime.combine(datetime.date.today(), wake_time) + datetime.timedelta(hours=2)).time() 
 
     #We need to shift times so that the day begins a midnight (and thus is on the same day) sleep start flex begins on 
-    accelDf.loc[:, "Shifted Time"] = pd.to_datetime(accelDf['UTC time']) - (datetime.datetime.combine(datetime.date.min, sleepEndFlex) - datetime.datetime.min)
+    accelDf.loc[:, "Shifted Time"] = pd.to_datetime(accelDf['Time']) - (datetime.datetime.combine(datetime.date.min, sleepEndFlex) - datetime.datetime.min)
     accelDf.loc[:, "Shifted Day"] = pd.to_datetime(accelDf['Shifted Time']).dt.date 
     #Also adjust bed/wake/flex times to account for shift
     
@@ -82,7 +81,7 @@ def activities(sensor_data, dates):
     sleepEndFlexShifted = (datetime.datetime.combine(datetime.date.today(), sleepEndFlex) - (datetime.datetime.combine(datetime.date.min, sleepEndFlex) - datetime.datetime.min)).time()
     
     
-    daily_activity_data = []
+    _sleep_periods = []
     for day, df in accelDf.groupby('Shifted Day'):
         
         #Keep track of how many 10min blocks are 1. inactive during "active" periods; or 2. active during "inactive periods"
@@ -92,88 +91,28 @@ def activities(sensor_data, dates):
             normal_time = t + (datetime.datetime.combine(datetime.date.min, sleepEndFlex) - datetime.datetime.min)
 
             #Ignore block if can't map to mean value
-            if len(df10min.loc[df10min['Time'] == normal_time.time(), 'Magnitude'].values) == 0:
+            if len(df10min.loc[df10min['time'] == normal_time.time(), 'magnitude'].values) == 0:
                 continue 
-             
+            
+            day_sleep_period_start = None
             if (sleepStartFlexShifted <= t.time() < sleepStartShifted) or (sleepEndShifted <= t.time()):
-                if tDf['magnitude'].abs().mean() < df10min.loc[df10min['Time'] == normal_time.time(), 'Magnitude'].values[0]: 
+                if tDf['magnitude'].abs().mean() < df10min.loc[df10min['time'] == normal_time.time(), 'magnitude'].values[0]: 
                     night_inactivity_count += 1
-                    
+                    if day_sleep_period_start is None:
+                        day_sleep_period_start = normal_time
+                
             elif sleepStartShifted <= t.time() < sleepEndShifted:
-                if tDf['magnitude'].abs().mean() > df10min.loc[df10min['Time'] == normal_time.time(), 'Magnitude'].values[0]: 
+                if tDf['magnitude'].abs().mean() > df10min.loc[df10min['time'] == normal_time.time(), 'magnitude'].values[0]: 
                     night_activity_count += 1
 
-            else: 
-                if tDf['magnitude'].abs().mean() < df10min.loc[df10min['Time'] == normal_time.time(), 'Magnitude'].values[0]:
-                    day_inactivity_count += 1
-
-        #Calculate day's sleep using these activity account
-        
+        #Calculate day's sleep duration using these activity account
         daily_sleep = (datetime.datetime.combine(datetime.date.today(), datetime.time(hour=8)) - datetime.timedelta(minutes = night_activity_count * 10) + datetime.timedelta(minutes = night_inactivity_count * 10)).time()
-        daily_sedentary = datetime.time(hour = int(day_inactivity_count * 10 / 60), 
-                                        minute = (day_inactivity_count * 10) % 60)
-        
-        #Label rest of the day as being active
-        h1, m1, s1 = daily_sleep.hour, daily_sleep.minute, daily_sleep.second
-        h2, m2, s2 = daily_sedentary.hour, daily_sedentary.minute, daily_sedentary.second
-        t1_secs, t2_secs = s1 + 60 * (m1 + 60 * h1), s2 + 60 * (m2 + 60 * h2) 
+      
+        #Set sleep start time 
+        if day_sleep_period_start is None: continue
+        if day_sleep_period_start.time() < datetime.time(hour=8): sleep_period_timestamp = datetime.datetime.combine(day + datetime.timedelta(days=1), day_sleep_period_start).timestamp() * 1000
+        else: sleep_period_timestamp = datetime.datetime.combine(day, day_sleep_period_start.time()).timestamp() * 1000
+        _sleep_period = {'start': sleep_period_timestamp, 'duration': daily_sleep}
+        _sleep_periods.append(_sleep_period)
 
-        SECS_IN_A_DAY = 86400
-        t3_secs = SECS_IN_A_DAY - (t1_secs + t2_secs)
-        #print(t3_secs)
-        daily_active = datetime.time(hour = int(t3_secs / 3600),
-                                     minute = int((t3_secs % 3600) / 60),
-                                     second = (t3_secs % 3600) % 60)
-        
-        daily_activity_data.append([datetime.datetime.combine(day, TIME), daily_sleep, daily_sedentary, daily_active])
-
-    return pd.DataFrame(daily_activity_data, columns = ["Date", "Sleep Duration", "Sedentary Duration", "Active Duration"])
-
-def convert_to_df(sensor_data):
-    """
-    Turn sensor data dict into df
-    """
-    sensorDf = sensor_data['lamp.accelerometer'].copy()
-        
-    sensorDf['UTC time'] = [str(d.date()) + "T" + str(d.time()) for d in sensorDf['local_datetime']]
-    sensorDf['Day'] = sensorDf.apply(lambda row: row['UTC time'].split('T')[0], axis=1)
-    sensorDf['Time'] = sensorDf.apply(lambda row: row['UTC time'].split('T')[1], axis=1)
-    sensorDf['Time'] = pd.to_datetime(sensorDf['Time']).dt.time
-    sensorDf['magnitude'] = sensorDf.apply(lambda row: np.linalg.norm([row['x'], row['y'], row['z']]), axis=1)
-    sensorDf.loc[:, 'magnitude'] = sensorDf.loc[:, 'magnitude'] - 1 
-    
-    return sensorDf
-
-def get_optimal_inactivity(df, length=8):
-    #Find maximum bout of inactivity
-    times = [(datetime.time(hour=h, minute=m).strftime("%H:%M"), (datetime.datetime.combine(datetime.date.today(), datetime.time(hour=h, minute=m)) + datetime.timedelta(hours=8, minutes=0)).strftime("%H:%M")) for h in range(18, 24)  for m in [0, 30] ] + [(datetime.time(hour=h, minute=m).strftime("%H:%M"), (datetime.datetime.combine(datetime.date.today(), datetime.time(hour=h, minute=m)) + datetime.timedelta(hours=8, minutes=0)).strftime("%H:%M")) for h in range(0, 4) for m in [0, 30] ]
-    mean_activity = float('inf')
-    for t0, t1 in times:
-        if "18:00" <= t0 <= "23:00":
-            selection = pd.concat([df.loc[datetime.datetime.strptime(t0, "%H:%M").time() <= df['Time'], :], df.loc[df['Time'] <= datetime.datetime.strptime(t1, "%H:%M").time(), :]])
-        else:
-            selection = df.loc[(datetime.datetime.strptime(t0, "%H:%M").time() <= df['Time']) & (df['Time'] <= datetime.datetime.strptime(t1, "%H:%M").time()), :]
-
-        sel_act = selection['magnitude'].abs().mean()
-
-        if sel_act < mean_activity:
-            mean_activity = sel_act
-            period = (t0, t1, sel_act)
-         
-    if mean_activity == float('inf'):
-        return None, None, None
-        #Default to midnight to 7am return ("00:00", "07:00", df['magnitude'].abs().mean())
-        
-    return period
-    
-def all(sensor_data, dates, resolution):
-    if 'lamp.accelerometer' not in sensor_data:
-        return pd.DataFrame({'Date': dates})
-
-    df_list = []
-    if resolution == datetime.timedelta(days=1):
-        activeDf = activities(sensor_data, dates)
-        df_list.append(activeDf)
-
-    allDfs = reduce(lambda left, right: pd.merge(left, right, on=["Date"], how='left'), df_list)
-    return allDfs
+    return _sleep_periods
---
+++ cortex/secondary/sleep_duration.py
@@ -0,0 +1,18 @@
+from ..feature_types import secondary_feature
+from ..primary.sleep_periods import sleep_periods
+
+import math 
+import pandas as pd
+import numpy as np
+import datetime
+from geopy import distance
+from functools import reduce
+
+@secondary_feature(
+    name="cortex.feature.sleep_duration",
+    dependencies=[sleep_periods]
+)
+def sleep_duration(**kwargs):
+    _sleep_periods = sleep_periods(id=kwargs['id'], start=kwargs['start'], end=kwargs['end'])
+    _sleep_duration = sum([res['duration'] for res in _sleep_periods])
+    return {'sleep_duration': _sleep_duration}
