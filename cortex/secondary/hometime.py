0d8304dc3176f0791df1accca67b4fd07899b2db
Ryan Hays <ryan.hays28@gmail.com>
2021-04-15 19:02:20 +0000

    Jewels a/b updated

--- cortex/secondary/hometime.py
+++ cortex/secondary/hometime.py
@@ -1,4 +1,40 @@
+from ..feature_types import secondary_feature
+from ..primary.significant_locations import significant_locations
 
+import math
+import datetime
+import pandas as pd
+
+MS_IN_A_DAY = 86400000
+@secondary_feature(
+    name='cortex.feature.entropy',
+    dependencies=[significant_locations]
+)
+def entropy(resolution=MS_IN_A_DAY, **kwargs):
+    """
+    Calculate entropy 
+    """
+    _significant_locations = significant_locations(id=kwargs['id'], start=kwargs['start'], end=kwargs['end'])
+    if len(_significant_locations['data']) == 0:
+        _entropy = None
+    _entropy = sum([loc['proportion'] * math.log(loc['proportion']) for loc in _significant_locations['data']])
+    if _entropy == 0: #no sig locs
+        _entropy = None
+    return {'timetamp':kwargs['start'], 'entropy': _entropy}
+
+
+@secondary_feature(
+    name='cortex.feature.hometime',
+    dependencies[significant_locations]
+)
+def hometime():
+    _significant_locations = significant_locations(id=kwargs['id'], start=kwargs['start'], end=kwargs['end'])
+    if len(_significant_locations['data']) == 0:
+        _hometime = None
+    _hometime = sum([loc['proportion'] * math.log(loc['proportion']) for loc in _significant_locations['data']])
+    if _entropy == 0: #no sig locs
+        _entropy = None
+    return {'timetamp':kwargs['start'], 'entropy': _entropy}
 
 def hometime(df, dates, resolution, k_max=10):
     _, df_sig_locs  = significant_locs(df, k_max=k_max)
