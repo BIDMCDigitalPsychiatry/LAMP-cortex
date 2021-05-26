d3a1f301a059bcaefb9e83c1dced11486478c27e
rydmel <rydmello@gmail.com>
2021-05-26 18:42:18 +0000

    fixed changes, tpyos

--- cortex/secondary/stationary_proportion.py
+++ cortex/secondary/stationary_proportion.py
@@ -1,10 +1,6 @@
 from ..feature_types import secondary_feature, log
 from ..raw.accelerometer import accelerometer
 import pandas as pd
-import numpy as np
-
-MS_IN_A_DAY = 86400000
-
 
 @secondary_feature(
     name='cortex.feature.stationary_proportion',
@@ -14,7 +10,6 @@ def stationary_proportion(g=9.57, eps=.1, col='z', **kwargs):
     """
     Compute the proportion of "stationary" accelerometer readings.
     An accelerometer reading is "stationary" if the z component is close to gravitational acceleration (9.81 m/s^2)
-    
     g : float
         Gravitational acceleration constant
     eps : float
@@ -22,15 +17,13 @@ def stationary_proportion(g=9.57, eps=.1, col='z', **kwargs):
     col : string
         Column name
     """
-    log.info(f'Loading Accelerometer data for 1st trajectory...')
+    #log.info(f'Loading Accelerometer data for 1st trajectory...')
     acc = accelerometer(**kwargs)['data']
     if acc:
         acc = pd.DataFrame(acc)[['x', 'y', 'z']]
         n = len(acc)
         ct = len(acc[(acc[col] >= g - eps) & (acc[col] <= g + eps)])
-        stationary_proportion = round(ct/n, 5)    
+        stationary_proportion = round(ct/n, 5)
     else:
         stationary_proportion = []
-    
-    
     return {'timestamp':kwargs['start'], 'stationary_proportion': stationary_proportion}
