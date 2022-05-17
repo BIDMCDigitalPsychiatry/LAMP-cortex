44d3f6d1d2256ca47b6a7d782d935dea1204b845
carlan1 <clangholm1@gmail.com>
2022-05-17 17:26:41 +0000

    update 3

--- tests/secondary_feature_tests.py
+++ tests/secondary_feature_tests.py
@@ -113,6 +113,23 @@ class TestSecondary(unittest.TestCase):
         self.assertEqual(ret0[3]['value'], 131120)
         self.assertEqual(ret0[4]['value'], 179956)
         self.assertEqual(ret0[5]['value'], 282319)
+        
+    def test_call_duration(self):
+        # Test that call duration works
+        import numpy as np
+        options = ["incoming", "outgoing", "all"]
+        for option in options:
+            ret = np.sum(event['value'] for event in cortex.secondary.call_duration.call_duration(
+                                           incoming = "all",
+                                           id="U26468383",
+                                           start=1584137124130,
+                                           end=1583532324130 + 30 * 60 * 60 * 24 * 1000,
+                                           resolution=60 * 60 * 24 * 1000,
+                                           feature="telephony")['data'] if event['value']!=None)
+            
+            goal = np.sum(call['data']['duration'] for call in LAMP.SensorEvent.all_by_participant(self.TEST_PARTICIPANT, origin='lamp.telephony')['data'] if call['data']['type'] == option)
+
+            self.assertEqual(ret, goal)
 
 if __name__ == '__main__':
     unittest.main()
