3990f287ac61e7a74b06095b09840f9dc2d83c59
carlan1 <clangholm1@gmail.com>
2022-10-26 20:30:19 +0000

    call degree update

--- cortex/secondary/call_degree.py
+++ cortex/secondary/call_degree.py
@@ -1,15 +1,15 @@
-""" Module for call_degree from raw feature calls """
+""" Module for call_degree from raw feature telephony """
 import numpy as np
 
 from ..feature_types import secondary_feature
-from ..raw.calls import calls
+from ..raw.telephony import telephony
 
 MS_IN_A_DAY = 86400000
 @secondary_feature(
     name='cortex.feature.call_degree',
-    dependencies=[calls]
+    dependencies=[telephony]
 )
-def call_degree(**kwargs):
+def call_degree(call_direction='all', **kwargs):
     """Returns the number of unique phone numbers a participant called
 
     Args:
@@ -25,6 +25,17 @@ def call_degree(**kwargs):
             timestamp (int): The beginning of the window (same as kwargs['start']).
             value (float): The number of unique phone numbers.
     """
-    _calls = calls(**kwargs)['data']
-    _call_degree = np.unique([call['call_trace'] for call in _calls]).size
-    return {'timestamp':kwargs['start'], 'value': _call_degree}
+    _calls = telephony(**kwargs)['data']
+    # if no data, return none
+    if len(_calls) == 0:
+        _call_degree = None
+    # if there is data, but not of the specified type, return 0
+    elif call_direction not in ['incoming', 'outgoing', 'all']:
+        raise Exception(f"{call_direction} is not a proper argument. "
+                        + "Must be incoming, outgoing, or all")
+    elif call_direction == 'all':
+        _call_degree = np.unique([call['trace'] for call in _calls]).size
+    else:
+        _call_degree = np.unique([call['trace'] for call in _calls
+                                  if call['type'] == call_direction]).size
+    return {'timestamp': kwargs['start'], 'value': _call_degree}
